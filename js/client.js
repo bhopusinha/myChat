66d5085833f841bb02570618a3952bcb17c56402
bhopusinha <108219196+bhopusinha@users.noreply.github.com>
2023-03-31 09:11:37 +0530

    Update client.js

--- js/client.js
+++ js/client.js
@@ -1,4 +1,4 @@
-const socket=io('http://localhost:8080')
+const socket=io('http://localhost:8000')
 
 const form =document.getElementById('send-container')
 const messageInp=document.getElementById('messageInp')
