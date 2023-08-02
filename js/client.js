037081f8e0cd05a966a49f8f6689cff86f2db9b8
bhopusinha <108219196+bhopusinha@users.noreply.github.com>
2023-08-03 01:25:14 +0530

    Update client.js

--- js/client.js
+++ js/client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:8000');
+const socket = io('https://your-backend.herokuapp.com');
 
 // get DOM element in respective js variables 
 const form=document.getElementById('send-container');
